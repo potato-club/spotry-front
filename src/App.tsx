6db03253768acb0fc269da52311507dd5b4b550d
JaiIn <di0308@naver.com>
2024-08-24 19:01:48 +0900

    초기 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
-import React from 'react';
-import './App.css';
 
 function App() {
   return (
-    <div className="bg-blue-500 text-white p-6">
-      <h1 className="text-3xl font-bold">Hello, Tailwind CSS with TypeScript!</h1>
+    <div>
+      <p>ㅇㅇㅇ</p>
     </div>
   );
 }
