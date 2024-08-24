a6e5338057b82d07cb13466e91aebb070537b0a2
JaiIn <di0308@naver.com>
2024-08-24 20:08:06 +0900

    style: 전역 스타일

---
+++ src/styles/Container.ts
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    width: 414px;
+    background-color: #333333;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    height: 100vh;
+`
+
+export const Btn = styled.div`
+cursor: pointer;
+background-color: #C1F84D;
+border-radius: 10px;
+width: 300px;
+display: flex;
+flex-direction: row;
+justify-content: center;
+align-items: center;
+height: 44px;
+`
