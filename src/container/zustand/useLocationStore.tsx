87f306393b92e719e80ad2d39443ed3d2101c8ab
JaiIn <di0308@naver.com>
2024-09-30 13:37:38 +0900

    chore: 삭제

--- src/container/zustand/useLocationStore.tsx
+++
@@ -1,7 +0,0 @@
-import { create } from "zustand";
-
-
-
-const useLocationStore = create();
-
-export default useLocationStore;
--- src/types/Location.ts
+++
@@ -1,6 +0,0 @@
-export interface Location{
-    city: string,
-    district: string,
-    neighborhood: string,
-    address: string,   
-}
