b48e9ee414aa2b9bd426f2b3ad3746646b36cd68
SeongJuLee0206 <dltjdwnchlrh3432@gmail.com>
2024-12-04 18:48:51 +0900

    "Fix Routes 수정"

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,12 @@ import Main from "./container/MainPage/Main";
 import { Container } from "./styles/Container";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import EachPage from "./container/MainPage/eachPage/EachPage";
+import LoginPage from "./components/Login/LoginPage";
+import MainBar from "./components/MenuBar/MainBar";
+import SubmitMain from "./components/SubmitLogin/SubmitPage";
+import WritePost from "./components/WritingPost/WritePost";
+import WrittenBtn from "./container/MainPage/mainDocument/WrittenBtn";
+import WriteButton from "./components/WritingPost/WriteButton";
 
 function App() {
   return (
@@ -11,10 +17,13 @@ function App() {
       <div className="main">
         <Container>
           <Routes>
-            <Route path="/" element={<MainLocation />} />
+            {/* <Route path="/" element={<MainLocation />} />
             <Route path="/main" element={<Main />} />
             <Route path="/search" element={<SearchPage />} />
-            <Route path="/each" element={<EachPage />} />
+            <Route path="/each" element={<EachPage />} /> */}
+            <Route path="/submit" element={<SubmitMain />} />
+            <Route path="/write" element={<WritePost />} />
+            <Route path="/writebtn" element={<WriteButton />} />
           </Routes>
         </Container>
       </div>
