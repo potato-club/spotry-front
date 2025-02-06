64281f8113f763d675db5071d3c154bfd049247d
Choi Dongin <di0308@naver.com>
2025-02-06 14:53:38 +0900

    feat: cookieUtil

--- src/api/cookie.ts
+++ src/api/cookie.ts
@@ -1,3 +1,17 @@
 import { Cookies } from "react-cookie";
 
 const cookies = new Cookies();
+
+export const setCookie = (name:string, value:string, dasy:number = 1) => {
+    const expires = new Date();
+    expires.setUTCDate(expires.getUTCDate() + dasy);
+    return cookies.set(name, value,{path:'/'});
+}
+
+export const getCookie = (name:string) => {
+    return cookies.get(name);
+}
+
+export const deleteCookie = (name:string, path = "/") => {
+    cookies.remove(name,{path});
+}
