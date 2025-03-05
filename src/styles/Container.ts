e005435282a9a38e113e40e49b25f45edb786b36
SeongJuLee0206 <dltjdwnchlrh3432@gmail.com>
2025-03-05 19:13:20 +0800

    "Feat: 비밀번호변경을위한 유저인증기능능"

--- src/styles/Container.ts
+++ src/styles/Container.ts
@@ -1,21 +1,21 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-width: 375px;
-background-color: #333333;
-display: flex;
-flex-direction: column;
-align-items: center;
-box-sizing: border-box;
-height: 100vh;
-`
+  width: 375px;
+  background-color: #333333;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  box-sizing: border-box;
+  height: 100vh;
+`;
 
-export const Btn = styled.div`
-cursor: pointer;
-background-color: #C1F84D;
-border-radius: 10px;
-display: flex;
-flex-direction: row;
-justify-content: center;
-align-items: center;
-`
+export const Btn = styled.button`
+  cursor: pointer;
+  background-color: #c1f84d;
+  border-radius: 10px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+`;
